8153af13dbdcbdef2721011f3e8ee6fb755225fc
dutangc <dutangc@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-02-06 16:47:39 +0000

    add a new way to compute matrix exponential through diagonalisation. if not, algorithm remains the same, i.e. Pade s approximation.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/expm@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/expm.h
+++ pkg/src/expm.h
@@ -1,2 +1,12 @@
+#include <R.h>
+#include <Rinternals.h>
+
 SEXP do_expm(SEXP x);
 void expm(double *x, int n, double *z);
+
+extern void
+F77_NAME(zgemm)(const char *transa, const char *transb, const int *m,
+	    const int *n, const int *k, const Rcomplex *alpha,
+	    const Rcomplex *a, const int *lda,
+	    const Rcomplex *b, const int *ldb,
+	    const Rcomplex *beta, Rcomplex *c, const int *ldc);
