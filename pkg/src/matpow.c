ff0e6e303299aa36d840c1d952fe2f5f3c7b635a
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-02-23 15:10:17 +0000

    matpow() ``the right way'' --- lifted from actuar/src/util.c (and prettified)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/expm@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/matpow.c
@@ -0,0 +1,66 @@
+/* --- this is basically a copy from  the code in  CRAN package actuar's
+ * src/util.c  (by Vincent Goulet)
+ * with slight shortcuts by Martin Maechler: */
+
+#include <R.h>
+#include <R_ext/BLAS.h>
+
+/* Power of a matrix x^k := x x ... x, where x in an (n x n) matrix
+ * and k is an *integer* (including -1). This function is fairly naive
+ * with little error checking since it is currently used in a very
+ * narrow and already controlled context.
+ */
+void matpow(double *x, int n, int k, double *z);
+
+int F77_SUB(matpowf)(double *x, int *n, int *k, double *z)
+{
+    int kk = *k; /* pass an 'own' integer; it will be modified below! */
+    matpow(x, *n, kk, z);
+    return 0;
+}
+
+void matpow(double *x, int n, int k, double *z)
+{
+    if (k == 0) { /* Return identity matrix */
+	int i, j;
+	for (i = 0; i < n; i++)
+	    for (j = 0; j < n; j++)
+		z[i * n + j] = (i == j) ? 1.0 : 0.0;
+	return;
+    }
+    else if (k < 0) {
+
+	REprintf("If you *REALLY* need <matrix> ^ (-n), use (m^n)^(-1) yourself!");
+    }
+    else { /* k >= 1 */
+
+	static const char *transa = "N";
+	static const double one = 1.0, zero = 0.0;
+	int nSqr = n * n;
+	double /* temporary matrix */
+	    *tmp  = (double *) R_alloc(nSqr, sizeof(double));
+
+	/* Take powers in multiples of 2 until there is only one
+	 * product left to make. That is, if k = 5, compute (x * x),
+	 * then ((x * x) * (x * x)) and finally ((x * x) * (x * x)) *
+	 * x. Idea taken from Octave in file .../src/xpow.cc. */
+	Memcpy(z, x, (size_t) nSqr);
+
+	k--;
+	while (k > 0) {
+	    if (k & 1) {	/* z := z * x */
+		F77_CALL(dgemm)(transa, transa, &n, &n, &n, &one,
+				z, &n, x, &n, &zero, tmp, &n);
+		Memcpy(z, tmp, (size_t) nSqr);
+	    }
+
+	    if(k == 1)
+		break;
+	    k >>= 1; /* efficient division by 2 ; now have k >= 1 */
+	    /* x := x * x */
+	    F77_CALL(dgemm)(transa, transa, &n, &n, &n, &one,
+			    x, &n, x, &n, &zero, tmp, &n);
+	    Memcpy(x, tmp, (size_t) nSqr);
+	}
+    }
+}
